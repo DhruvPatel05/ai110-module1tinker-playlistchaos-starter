d91d317637cad0533cb3e351cb9729b28efd1275
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-08 09:04:26 -0700

    Settings View UI updated.

--- HikeApp/HikeApp/Screens/SettingsView.swift
+++ HikeApp/HikeApp/Screens/SettingsView.swift
@@ -10,33 +10,46 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         // MARK: - SECTION: HEADER
-        HStack {
-            Spacer()
-            Image(systemName: "laurel.leading")
-                .font(.system(size: 80,weight: .black))
+        VStack {
+            HStack {
+                Spacer()
+                Image(systemName: "laurel.leading")
+                    .font(.system(size: 80,weight: .black))
+                
+                VStack(spacing: -10) {
+                    Text("Hike")
+                        .font(.system(size: 66,weight: .black))
+                    Text("Editors' Choice ")
+                }
+                Image(systemName: "laurel.trailing")
+                    .font(.system(size: 80,weight: .black))
+                Spacer()
+            }
+            .foregroundStyle(LinearGradient(colors:[.customGreenLight,.customGreenMedium,.customGreenDark
+                                                   ],
+                                            startPoint: .top,
+                                            endPoint: .bottom
+                                           )
+            )
+            .padding(.top,8)
             
-            Text("Hike")
-                .font(.system(size: 66,weight: .black))
-            Image(systemName: "laurel.trailing")
-                .font(.system(size: 80,weight: .black))
-            Spacer()
+            Text("Where can you find \nperfect tracks")
+                .font(.title2)
+                .fontWeight(.heavy)
+            
+            // MARK: - SECTION Icons
+            
+            // MARK: - SECTION About
+            
+            // MARK: List
+            
         }
-        .foregroundStyle(LinearGradient(colors:[.customGreenLight,.customGreenMedium,.customGreenDark
-                                               ],
-            startPoint: .top,
-            endPoint: .bottom)
-        )
-        
-        // MARK: - SECTION Icons
-        
-        // MARK: - SECTION About
-        
-        // MARK: List
-        
     }
 }
 
-#Preview {
-    
-    SettingsView()
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
 }
+
