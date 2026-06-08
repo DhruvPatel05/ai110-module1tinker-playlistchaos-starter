5580a9f91536b58d74715a484e1c4d830251d258
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-08 07:56:31 -0700

    Merge remote history

---
+++ HikeApp/HikeApp/Screens/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  HikeApp
+//
+//  Created by Dhruv Patel on 03/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+       CardView()
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
