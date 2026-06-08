a47ec270deabf56abce4e0428b08328c058bfa0f
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-08 08:14:36 -0700

    Settings View Added

---
+++ HikeApp/HikeApp/Screens/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  HikeApp
+//
+//  Created by Dhruv Patel on 08/06/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
