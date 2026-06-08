228ae28202cc51937cca069fdfb6f5f940f4edb9
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-08 08:46:52 -0700

    Setting Screen UI updated.

--- HikeApp/HikeApp/Screens/SettingsView.swift
+++ HikeApp/HikeApp/Screens/SettingsView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Settings View")
+        // MARK: - SECTION: HEADER
+        HStack {
+            Spacer()
+            Image(systemName: "laurel.leading")
+                .font(.system(size: 80,weight: .black))
+            
+            Text("Hike")
+                .font(.system(size: 66,weight: .black))
+            Image(systemName: "laurel.trailing")
+                .font(.system(size: 80,weight: .black))
+            Spacer()
+        }
+        .foregroundStyle(LinearGradient(colors:[.customGreenLight,.customGreenMedium,.customGreenDark
+                                               ],
+            startPoint: .top,
+            endPoint: .bottom)
+        )
+        
+        // MARK: - SECTION Icons
+        
+        // MARK: - SECTION About
+        
+        // MARK: List
+        
     }
 }
 
 #Preview {
+    
     SettingsView()
 }
